2a372e49cfa3e0174d0d5702d92d00da511b5087
Tharushika251 <tharushikarukshani10@gmail.com>
2024-10-13 17:53:27 +0530

    chatmsg

--- Backend/models/ChatMessage.js
+++ Backend/models/ChatMessage.js
@@ -4,13 +4,8 @@ const mongoose = require('mongoose');
 const ChatMessageSchema = new mongoose.Schema({
     sender: { type: String, required: true },
     receiver: { type: String, required: true },
-<<<<<<< Updated upstream
-    message: { type: String, required: false }, // Set required to false for image messages
-    file: { type: String }, // Add file field to store file URL
-=======
     message: { type: String, required: true },
     file: { type: String }, // URL for image attachment
->>>>>>> Stashed changes
     timestamp: { type: Date, default: Date.now }
 });
 
